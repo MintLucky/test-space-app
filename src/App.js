08df8b31f217e3c81ca9fbd54a03177fa1c50ee3
Alex <Alexander.Bashlykov@sitewerk.ch>
2025-02-23 14:41:15 -0300

    install prettier and run for all files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { useState } from 'react';
-import { ReactComponent as ShoppingCartImage } from './images/shopping-cart-gradient.svg';
+import React, { useState } from "react";
+import { ReactComponent as ShoppingCartImage } from "./images/shopping-cart-gradient.svg";
 
-import './App.scss';
+import "./App.scss";
 
-import { ReactComponent as LogoImage } from './images/logo.svg';
-import { ReactComponent as RocketImage } from './images/rocket.svg';
-import EarthImage from './images/earth.png';
-import CardImage1 from './images/card1-bg.jpg';
-import CardImage2 from './images/card2-bg.jpg';
-import CardImage3 from './images/card3-bg.jpg';
-import CardImage4 from './images/card4-bg.jpg';
-  
+import { ReactComponent as LogoImage } from "./images/logo.svg";
+import { ReactComponent as RocketImage } from "./images/rocket.svg";
+import EarthImage from "./images/earth.png";
+import CardImage1 from "./images/card1-bg.jpg";
+import CardImage2 from "./images/card2-bg.jpg";
+import CardImage3 from "./images/card3-bg.jpg";
+import CardImage4 from "./images/card4-bg.jpg";
 
 const cardsContent = [
   {
@@ -39,20 +38,28 @@ const cardsContent = [
     image: CardImage4,
     isBig: true,
   },
-]
-
+];
 
 const Card = ({ title, subtitle, image, isBig = false }) => {
   return (
-    <div className="card" style={{ backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(15, 20, 32, 0.79) 100%), url(${image})` }}>
+    <div
+      className="card"
+      style={{
+        backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(15, 20, 32, 0.79) 100%), url(${image})`,
+      }}
+    >
       <div className="card__content">
-        <h3 className={"card__title " + (isBig ? "card__title--big" : "")}>{title}</h3>
-        <p className={"card__subtitle " + (isBig ? "card__subtitle--big" : "")}>{subtitle}</p>
+        <h3 className={"card__title " + (isBig ? "card__title--big" : "")}>
+          {title}
+        </h3>
+        <p className={"card__subtitle " + (isBig ? "card__subtitle--big" : "")}>
+          {subtitle}
+        </p>
         <button className="button">Learn more</button>
       </div>
     </div>
   );
-}
+};
 
 function App() {
   const [isMenuOpened, setIsMenuOpened] = useState(false);
@@ -64,10 +71,18 @@ function App() {
           <a href="#" className="header__logo">
             <LogoImage />
           </a>
-          <nav className={`nav ${isMenuOpened ? 'nav--open' : ''}`}>
+          <nav className={`nav ${isMenuOpened ? "nav--open" : ""}`}>
             <ul className="nav__list">
-              <li className="nav__item"><a href="#" className="nav__link">Home</a></li>
-              <li className="nav__item"><a href="#" className="nav__link">Products</a></li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Home
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="#" className="nav__link">
+                  Products
+                </a>
+              </li>
               <li className="nav__item">
                 <a href="#" className="nav__link">
                   <div className="header__cart">
@@ -77,10 +92,12 @@ function App() {
               </li>
             </ul>
           </nav>
-          
-          <button 
-            className={"burger-menu " + (isMenuOpened ? " burger-menu--open" : "")} 
-            onClick={() => setIsMenuOpened(opened => !opened)}
+
+          <button
+            className={
+              "burger-menu " + (isMenuOpened ? " burger-menu--open" : "")
+            }
+            onClick={() => setIsMenuOpened((opened) => !opened)}
           >
             <span className="burger-menu__line"></span>
             <span className="burger-menu__line"></span>
@@ -90,12 +107,16 @@ function App() {
       </header>
 
       <section className="hero container-big">
-        <div className='hero__content-wrapper container'>
+        <div className="hero__content-wrapper container">
           <div className="hero__content">
             <h1 className="hero__title">
-              Discover the vast expanses of <span className="hero__title--highlight">space</span>
+              Discover the vast expanses of{" "}
+              <span className="hero__title--highlight">space</span>
             </h1>
-            <p className="hero__subtitle">Where the possibilities are <span className="hero__subtitle--highlight">endless</span>!</p>
+            <p className="hero__subtitle">
+              Where the possibilities are{" "}
+              <span className="hero__subtitle--highlight">endless</span>!
+            </p>
             <button className="hero__button button">Learn more</button>
           </div>
           <div className="hero__image">
@@ -108,34 +129,57 @@ function App() {
       <section className="offers container">
         <h2 className="offers__title">Offers</h2>
         <div className="card-grid">
-          {cardsContent && cardsContent.map((card) => (
-            <div key={card.id} className="card-grid__item">
-              <Card 
-                title={card.title}
-                subtitle={card.subtitle}
-                image={card.image}
-                isBig={card?.isBig}
-              />
-            </div>
-          ))}
+          {cardsContent &&
+            cardsContent.map((card) => (
+              <div key={card.id} className="card-grid__item">
+                <Card
+                  title={card.title}
+                  subtitle={card.subtitle}
+                  image={card.image}
+                  isBig={card?.isBig}
+                />
+              </div>
+            ))}
         </div>
       </section>
 
       <section className="journey container">
         <h2 className="journey__title">Embark on a space journey</h2>
         <p className="journey__text">
-          Traveling into space is one of the most exciting and unforgettable adventures that can change your life forever. And if you have ever dreamed of exploring stars, planets and galaxies, then our company is ready to help you make this dream come true. Our space tourism program offers a unique opportunity to see the Earth from a height of several hundred kilometers and feel that every moment is spent with incredible impressions. Experience and see for yourself! Our ship is equipped with the most modern technology and comfort so that you can fully enjoy your adventure in space. We offer several different tour packages that will suit the needs of both beginners and experienced space travelers.
+          Traveling into space is one of the most exciting and unforgettable
+          adventures that can change your life forever. And if you have ever
+          dreamed of exploring stars, planets and galaxies, then our company is
+          ready to help you make this dream come true. Our space tourism program
+          offers a unique opportunity to see the Earth from a height of several
+          hundred kilometers and feel that every moment is spent with incredible
+          impressions. Experience and see for yourself! Our ship is equipped
+          with the most modern technology and comfort so that you can fully
+          enjoy your adventure in space. We offer several different tour
+          packages that will suit the needs of both beginners and experienced
+          space travelers.
         </p>
         <p className="journey__text journey__text-extra">
-          Traveling into space is one of the most exciting and unforgettable adventures that can change your life forever. And if you have ever dreamed of exploring stars, planets and galaxies, then our company is ready to help you make this dream come true. Our space tourism program offers a unique opportunity to see the Earth from a height of several hundred kilometers and feel that every moment is spent with incredible impressions. Experience and see for yourself! Our ship is equipped with the most modern technology and comfort so that you can fully enjoy your adventure in space. We offer several different tour packages that will suit the needs of both beginners and experienced space travelers.
+          Traveling into space is one of the most exciting and unforgettable
+          adventures that can change your life forever. And if you have ever
+          dreamed of exploring stars, planets and galaxies, then our company is
+          ready to help you make this dream come true. Our space tourism program
+          offers a unique opportunity to see the Earth from a height of several
+          hundred kilometers and feel that every moment is spent with incredible
+          impressions. Experience and see for yourself! Our ship is equipped
+          with the most modern technology and comfort so that you can fully
+          enjoy your adventure in space. We offer several different tour
+          packages that will suit the needs of both beginners and experienced
+          space travelers.
         </p>
         <input type="checkbox" id="journey_btn" />
         <label for="journey_btn"></label>
-        </section>
+      </section>
 
       <footer className="footer">
         <div className="footer__content container">
-          <div className="footer__icon"><RocketImage /></div>
+          <div className="footer__icon">
+            <RocketImage />
+          </div>
           <p className="footer__text">Exciting space adventures!</p>
         </div>
       </footer>
